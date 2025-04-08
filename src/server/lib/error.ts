cca0bc04ba343a3360e51606a592585c4c2d0a4d
Tyler Haas <tylerh@articulate.com>
2025-04-07 20:19:52 -0700

    log message on error handler

--- src/server/lib/error.ts
+++ src/server/lib/error.ts
@@ -3,7 +3,7 @@ import logger from './logger'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function globalErrorHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void {
-  logger.error(err.stack)
+  logger.error(err.message)
   res.status(500).json({
     error: {
       message: process.env.NODE_ENV === 'production' ? 'Internal Server Error' : err.message,
